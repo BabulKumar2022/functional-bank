3eab1c977e1fd22e5138123848516d72bd13c0d5
BabulKumar2022 <bb3583400@gmail.com>
2022-10-15 17:21:10 +0600

    function

--- js/banking.js
+++ js/banking.js
@@ -1,1 +1,24 @@
-console.log('connected')
+
+document.getElementById('deposit-submit').addEventListener('click', function(){
+    // console.log('deposit-button clicked')
+    const depositInput = document.getElementById('deposit-input');
+    const depositAmountText = depositInput.value;
+    const depositAmount = parseInt(depositAmountText)
+    // console.log(depositAmount);
+
+    //get current deposit
+    const depositTotal = document.getElementById('deposit-total');
+    const depositTotalText = depositTotal.innerText;
+    const previousDepositTotal = parseInt(depositTotalText)
+    // console.log(depositTotalText);
+    depositTotal.innerText = previousDepositTotal + depositAmount;
+
+    //update balance
+
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseInt(balanceTotalText);
+    balanceTotal.innerText = previousBalanceTotal + depositAmount;
+    //clear input field
+    depositInput.value = ''
+})
