e2c1a62e71a72fe3f9b6c05998071aefa5f85f5e
BabulKumar2022 <bb3583400@gmail.com>
2022-10-15 19:21:40 +0600

    ok

--- js/banking.js
+++ js/banking.js
@@ -13,7 +13,7 @@ document.getElementById('deposit-submit').addEventListener('click', function(){
     // console.log(depositTotalText);
     depositTotal.innerText = previousDepositTotal + depositAmount;
 
-    //update balance
+    // update balance via deposit
 
     const balanceTotal = document.getElementById('balance-total');
     const balanceTotalText = balanceTotal.innerText;
@@ -21,4 +21,31 @@ document.getElementById('deposit-submit').addEventListener('click', function(){
     balanceTotal.innerText = previousBalanceTotal + depositAmount;
     //clear input field
     depositInput.value = ''
+})
+
+// function for withdraw
+
+document.getElementById('withdraw-submit').addEventListener('click', function(){
+    // console.log('clicked')
+    const withdrawInput = document.getElementById('withdraw-input');
+    const withdrawAmountText = withdrawInput.value;
+    // console.log(withdrawAmountText);
+    const withdrawAmount = parseInt(withdrawAmountText);
+
+    //get current withdraw
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const withdrawTotalText = withdrawTotal.innerText;
+    const previousWithdrawTotal =  parseInt(withdrawTotalText);
+    console.log(previousWithdrawTotal);
+    withdrawTotal.innerText = previousWithdrawTotal + withdrawAmount;
+
+    // update balance via withdraw
+
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseInt(balanceTotalText);
+    balanceTotal.innerText = previousBalanceTotal - withdrawAmount;
+    //clear input field
+
+    withdrawInput.value = '';
 })
